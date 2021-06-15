9380a29a0eba2101dada880c024bb209fd4ec09d
Zlenko <zlenko.d.s@gmail.com>
2021-06-15 20:47:51 +0300

    fix jump indicator

--- src/Pager.tsx
+++ src/Pager.tsx
@@ -1,26 +1,22 @@
 import * as React from 'react';
-import {
-  Animated,
-  PanResponder,
-  Keyboard,
-  StyleSheet,
-  GestureResponderEvent,
-  PanResponderGestureState,
-  I18nManager,
-  View,
-} from 'react-native';
+import { Animated, Keyboard, Platform, StyleSheet } from 'react-native';
+import ViewPager, {
+  PageScrollStateChangedNativeEvent,
+} from 'react-native-pager-view';
 import useAnimatedValue from './useAnimatedValue';
 import {
   NavigationState,
   Route,
-  Layout,
+  Listener,
   EventEmitterProps,
   PagerProps,
-  Listener,
 } from './types';
 
+const IS_IOS = Platform.OS === 'ios';
+
+const AnimatedViewPager = Animated.createAnimatedComponent(ViewPager);
+
 type Props<T extends Route> = PagerProps & {
-  layout: Layout;
   onIndexChange: (index: number) => void;
   navigationState: NavigationState<T>;
   children: (
@@ -38,18 +34,7 @@ type Props<T extends Route> = PagerProps & {
   ) => React.ReactElement;
 };
 
-const DEAD_ZONE = 12;
-
-const DefaultTransitionSpec = {
-  timing: Animated.spring,
-  stiffness: 1000,
-  damping: 500,
-  mass: 3,
-  overshootClamping: true,
-};
-
 export default function Pager<T extends Route>({
-  layout,
   keyboardDismissMode = 'auto',
   swipeEnabled = true,
   navigationState,
@@ -58,176 +43,79 @@ export default function Pager<T extends Route>({
   onSwipeEnd,
   children,
   style,
+  ...rest
 }: Props<T>) {
-  const { routes, index } = navigationState;
-
-  const panX = useAnimatedValue(0);
+  const { index } = navigationState;
 
   const listenersRef = React.useRef<Listener[]>([]);
 
+  const pagerRef = React.useRef<ViewPager>();
+  const indexRef = React.useRef<number>(index);
   const navigationStateRef = React.useRef(navigationState);
-  const layoutRef = React.useRef(layout);
-  const onIndexChangeRef = React.useRef(onIndexChange);
 
-  const currentIndexRef = React.useRef(index);
-  const pendingIndexRef = React.useRef<number>();
+  const position = useAnimatedValue(index);
+  const offset = useAnimatedValue(0);
 
-  const swipeVelocityThreshold = 0.15;
-  const swipeDistanceThreshold = layout.width / 1.75;
-
-  const jumpToIndex = React.useCallback(
-    (index: number) => {
-      const offset = -index * layoutRef.current.width;
-
-      const { timing, ...transitionConfig } = DefaultTransitionSpec;
-
-      Animated.parallel([
-        timing(panX, {
-          ...transitionConfig,
-          toValue: offset,
-          useNativeDriver: false,
-        }),
-      ]).start(({ finished }) => {
-        if (finished) {
-          onIndexChangeRef.current(index);
-          pendingIndexRef.current = undefined;
-        }
-      });
-
-      pendingIndexRef.current = index;
-    },
-    [panX]
-  );
+  // when swiping the next scene distance is always 1,
+  // but when pressing a tab can be greater
+  const nextSceneDistance = useAnimatedValue(1);
 
   React.useEffect(() => {
     navigationStateRef.current = navigationState;
-    layoutRef.current = layout;
-    onIndexChangeRef.current = onIndexChange;
   });
 
-  React.useEffect(() => {
-    const offset = -navigationStateRef.current.index * layout.width;
+  const jumpTo = React.useCallback(
+    (key: string) => {
+      const index = navigationStateRef.current.routes.findIndex(
+        (route: { key: string }) => route.key === key
+      );
 
-    panX.setValue(offset);
-  }, [layout.width, panX]);
+      if (IS_IOS) {
+        nextSceneDistance.setValue(Math.abs(indexRef.current - index));
+      }
+
+      pagerRef.current?.setPage(index);
+    },
+    [nextSceneDistance]
+  );
 
   React.useEffect(() => {
     if (keyboardDismissMode === 'auto') {
       Keyboard.dismiss();
     }
 
-    if (layout.width && currentIndexRef.current !== index) {
-      jumpToIndex(index);
+    if (indexRef.current !== index) {
+      pagerRef.current?.setPage(index);
     }
-  }, [jumpToIndex, keyboardDismissMode, layout.width, index]);
+  }, [keyboardDismissMode, index]);
 
-  const isMovingHorizontally = (
-    _: GestureResponderEvent,
-    gestureState: PanResponderGestureState
+  const onPageScrollStateChanged = (
+    state: PageScrollStateChangedNativeEvent
   ) => {
-    return (
-      Math.abs(gestureState.dx) > Math.abs(gestureState.dy * 2) &&
-      Math.abs(gestureState.vx) > Math.abs(gestureState.vy * 2)
-    );
-  };
+    const { pageScrollState } = state.nativeEvent;
 
-  const canMoveScreen = (
-    event: GestureResponderEvent,
-    gestureState: PanResponderGestureState
-  ) => {
-    if (swipeEnabled === false) {
-      return false;
-    }
+    switch (pageScrollState) {
+      case 'idle':
+        onSwipeEnd?.();
+        return;
+      case 'dragging': {
+        const subscription = offset.addListener(({ value }) => {
+          const next =
+            index + (value > 0 ? Math.ceil(value) : Math.floor(value));
 
-    return (
-      isMovingHorizontally(event, gestureState) &&
-      ((gestureState.dx >= DEAD_ZONE && currentIndexRef.current > 0) ||
-        (gestureState.dx <= -DEAD_ZONE &&
-          currentIndexRef.current < routes.length - 1))
-    );
-  };
+          if (next !== index) {
+            listenersRef.current.forEach((listener) => listener(next));
+          }
 
-  const startGesture = () => {
-    onSwipeStart?.();
+          offset.removeListener(subscription);
+        });
 
-    if (keyboardDismissMode === 'on-drag') {
-      Keyboard.dismiss();
-    }
-
-    panX.stopAnimation();
-    // @ts-expect-error: _value is private, but docs use it as well
-    panX.setOffset(panX._value);
-  };
-
-  const respondToGesture = (
-    _: GestureResponderEvent,
-    gestureState: PanResponderGestureState
-  ) => {
-    if (
-      // swiping left
-      (gestureState.dx > 0 && index <= 0) ||
-      // swiping right
-      (gestureState.dx < 0 && index >= routes.length - 1)
-    ) {
-      return;
-    }
-
-    if (layout.width) {
-      // @ts-expect-error: _offset is private, but docs use it as well
-      const position = (panX._offset + gestureState.dx) / -layout.width;
-      const next =
-        position > index ? Math.ceil(position) : Math.floor(position);
-
-      if (next !== index) {
-        listenersRef.current.forEach((listener) => listener(next));
+        onSwipeStart?.();
+        return;
       }
     }
-
-    panX.setValue(gestureState.dx);
   };
 
-  const finishGesture = (
-    _: GestureResponderEvent,
-    gestureState: PanResponderGestureState
-  ) => {
-    panX.flattenOffset();
-
-    onSwipeEnd?.();
-
-    const currentIndex =
-      typeof pendingIndexRef.current === 'number'
-        ? pendingIndexRef.current
-        : currentIndexRef.current;
-
-    let nextIndex = currentIndex;
-
-    if (
-      Math.abs(gestureState.dx) > Math.abs(gestureState.dy) &&
-      Math.abs(gestureState.vx) > Math.abs(gestureState.vy) &&
-      (Math.abs(gestureState.dx) > swipeDistanceThreshold ||
-        Math.abs(gestureState.vx) > swipeVelocityThreshold)
-    ) {
-      nextIndex = Math.round(
-        Math.min(
-          Math.max(
-            0,
-            currentIndex - gestureState.dx / Math.abs(gestureState.dx)
-          ),
-          routes.length - 1
-        )
-      );
-
-      currentIndexRef.current = nextIndex;
-    }
-
-    if (!isFinite(nextIndex)) {
-      nextIndex = currentIndex;
-    }
-
-    jumpToIndex(nextIndex);
-  };
-
-  // TODO: use the listeners
   const addEnterListener = React.useCallback((listener: Listener) => {
     listenersRef.current.push(listener);
 
@@ -240,85 +128,55 @@ export default function Pager<T extends Route>({
     };
   }, []);
 
-  const jumpTo = React.useCallback(
-    (key: string) => {
-      const index = navigationStateRef.current.routes.findIndex(
-        (route: { key: string }) => route.key === key
-      );
-
-      jumpToIndex(index);
-    },
-    [jumpToIndex]
-  );
-
-  const panResponder = PanResponder.create({
-    onMoveShouldSetPanResponder: canMoveScreen,
-    onMoveShouldSetPanResponderCapture: canMoveScreen,
-    onPanResponderGrant: startGesture,
-    onPanResponderMove: respondToGesture,
-    onPanResponderTerminate: finishGesture,
-    onPanResponderRelease: finishGesture,
-    onPanResponderTerminationRequest: () => true,
-  });
-
-  const maxTranslate = layout.width * (routes.length - 1);
-  const translateX = Animated.multiply(
-    panX.interpolate({
-      inputRange: [-maxTranslate, 0],
-      outputRange: [-maxTranslate, 0],
-      extrapolate: 'clamp',
-    }),
-    I18nManager.isRTL ? -1 : 1
-  );
+  React.useEffect(() => {
+    if (IS_IOS) {
+      nextSceneDistance.setValue(1);
+    }
+  }, [navigationState.index, nextSceneDistance]);
 
   return children({
-    position: layout.width
-      ? Animated.divide(panX, -layout.width)
-      : new Animated.Value(index),
+    position: Animated.add(
+      position,
+      Animated.multiply(offset, nextSceneDistance)
+    ),
     addEnterListener,
     jumpTo,
     render: (children) => (
-      <Animated.View
-        style={[
-          styles.sheet,
-          layout.width
-            ? {
-                width: routes.length * layout.width,
-                transform: [{ translateX }],
-              }
-            : null,
-          style,
-        ]}
-        {...panResponder.panHandlers}
+      <AnimatedViewPager
+        {...rest}
+        ref={pagerRef}
+        style={[styles.container, style]}
+        initialPage={index}
+        keyboardDismissMode={
+          keyboardDismissMode === 'auto' ? 'on-drag' : keyboardDismissMode
+        }
+        onPageScroll={Animated.event(
+          [
+            {
+              nativeEvent: {
+                position: position,
+                offset: offset,
+              },
+            },
+          ],
+          { useNativeDriver: true }
+        )}
+        onPageSelected={(e) => {
+          const index = e.nativeEvent.position;
+          indexRef.current = index;
+          onIndexChange(index);
+        }}
+        onPageScrollStateChanged={onPageScrollStateChanged}
+        scrollEnabled={swipeEnabled}
       >
-        {React.Children.map(children, (child, i) => {
-          const route = routes[i];
-          const focused = i === index;
-
-          return (
-            <View
-              key={route.key}
-              style={
-                layout.width
-                  ? { width: layout.width }
-                  : focused
-                  ? StyleSheet.absoluteFill
-                  : null
-              }
-            >
-              {focused || layout.width ? child : null}
-            </View>
-          );
-        })}
-      </Animated.View>
+        {children}
+      </AnimatedViewPager>
     ),
   });
 }
 
 const styles = StyleSheet.create({
-  sheet: {
+  container: {
     flex: 1,
-    flexDirection: 'row',
-    alignItems: 'stretch',
   },
 });
